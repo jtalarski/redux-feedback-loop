acc45c7a414b086a737186ab9195fcdb36faef71
Jim Talarski <jim.talarski@gmail.com>
2020-09-26 14:13:13 -0500

    basic get route working

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const feedback = require ('./modules/feedback.route')
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
+app.use('/feedback', feedback);
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
